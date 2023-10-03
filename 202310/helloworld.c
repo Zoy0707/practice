a5fa38209d6c0a2505dbbb06d88bbb419654826b
Zoy0707 <127084324+Zoy0707@users.noreply.github.com>
2023-10-03 16:36:17 +1100

    000

---
+++ 202310/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  // Include standard input/output library
+
+// The main function is the entry point of the program
+int main() {
+    // Print a message to the console
+    printf("Hello, world!\n");
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
